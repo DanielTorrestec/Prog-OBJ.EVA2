77e816990b2d77b24f19bcee86663b4198149004
DanielTorrestec <l25550429@chihuahua2.tecnm.mx>
2026-04-20 18:51:24 -0600

    PRACTICA 8

---
+++ EVA2_8_ABSTRACT/src/main/java/com/mycompany/eva2_8_abstract/EVA2_8_ABSTRACT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_8_abstract;
+
+/**
+ *
+ * @author chili
+ */
+public class EVA2_8_ABSTRACT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
