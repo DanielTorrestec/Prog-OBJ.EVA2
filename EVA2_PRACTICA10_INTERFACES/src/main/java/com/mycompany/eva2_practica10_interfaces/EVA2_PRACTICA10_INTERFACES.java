37f79b45749da3373599df8490aa30e6caa5b7a8
DanielTorrestec <l25550429@chihuahua2.tecnm.mx>
2026-04-20 18:52:08 -0600

    PRACTICA 10

---
+++ EVA2_PRACTICA10_INTERFACES/src/main/java/com/mycompany/eva2_practica10_interfaces/EVA2_PRACTICA10_INTERFACES.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_practica10_interfaces;
+
+/**
+ *
+ * @author chili
+ */
+public class EVA2_PRACTICA10_INTERFACES {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.mostrarMensaje();}
+        // Mensaje mensaje = new Mensaje();
+    }
+
+interface Prueba {
+    
+}
---
+++ EVA2_PRACTICA10_INTERFACES/src/main/java/com/mycompany/eva2_practica10_interfaces/Mensaje.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.eva2_practica10_interfaces;
+
+/**
+ *
+ * @author chili
+ */
+public interface Mensaje {
+    //int valor = 100;//normalmente no se usan mucho
+    void mostrarMensaje();//la interfaz solo tiene métodos publicos abstractos
+}
